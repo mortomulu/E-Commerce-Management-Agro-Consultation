0a3d85d573d69068bd18a7eddbf5ae90143b2e43
mortomulu <mfajrihidayat09@gmail.com>
2024-05-09 00:29:49 +0700

    ui : badge product card

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -3,29 +3,36 @@ import { FaRegHeart } from "react-icons/fa";
 import { Product } from "@/types/product";
 
 interface CardDataProps {
-    todo : Product
+  todo: Product;
 }
 
-const ProductCard : React.FC<CardDataProps> = ({
-    todo,
-}) => {
-    return (
-        <div className="card bg-base-100 shadow-xl p-0">
-           
-            <img className="rounded-t-lg max-h-40 object-cover max-w-full" src={todo.url_image} alt="Shoes" />
-           
-          <div className="card-body">
-            <h2 className="card-title flex justify-between truncate">
-            {todo.product_name}
-            <FaRegHeart />
-            </h2>
-            <p>$ {todo.price}</p>
-            <div className="card-actions justify-end">
-              <div className="badge badge-outline">Products</div>
-            </div>
+const ProductCard: React.FC<CardDataProps> = ({ todo }) => {
+  const badge = todo.product_category == "pra" ? "bg-green-500" : "bg-blue-500";
+
+  return (
+    <div className="card bg-base-100 shadow-xl p-0">
+      <img
+        className="rounded-t-[18px] max-h-40 object-cover max-w-full"
+        src={todo.url_image}
+        alt="Shoes"
+      />
+
+      <div className="card-body">
+        <div className="card-title flex justify-between truncate">
+          <h2 className="text-xl">{todo.product_name}</h2>
+          <FaRegHeart />
+        </div>
+        <p className="text-sm">$ {todo.price}</p>
+        <div className="card-actions justify-end">
+          <div
+            className={`${badge} badge badge-outline border-none pt-4 pb-4 px-5`}
+          >
+            {todo.product_category}
           </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
