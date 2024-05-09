0abb021ef536a4df505766905bdae47e8554356a
mortomulu <mfajrihidayat09@gmail.com>
2024-05-09 14:40:10 +0700

    ui : sidebar admin

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import SideNavbar from "@/components/Sidebar/SideNavbar"
+
+export default function DashboardPage () {
+    return (
+        <div className="h-screen">
+        <SideNavbar/>
+        </div>
+    )
+}
