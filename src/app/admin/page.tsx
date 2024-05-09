3cb49af91abf5ab796439c05c34d22cd2cad9ec7
mortomulu <mfajrihidayat09@gmail.com>
2024-05-09 15:47:35 +0700

    ui : card stats dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,11 @@
 import SideNavbar from "@/components/Sidebar/SideNavbar"
+import CardDataStatsContainer from "@/components/CardDataStats/CardDataStatsContainer"
 
 export default function DashboardPage () {
     return (
-        <div className="h-screen">
+        <div className="h-auto flex">
         <SideNavbar/>
+        <CardDataStatsContainer/>
         </div>
     )
 }
--- src/components/Sidebar/SideNavbar.tsx
+++ src/components/Sidebar/SideNavbar.tsx
@@ -7,29 +7,30 @@ import {   HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, 
 
 export default function SideNavbar() {
   return (
-    <Sidebar aria-label="Sidebar with content separator example h-screen">
-      <h1 className="text-2xl font-semibold mb-10 ml-4"> <span className="text-green-500">Agro</span>Merce</h1>
-      <Sidebar.Items>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiChartPie}>
-            Dashboard
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiUser}>
-            Users
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiShoppingBag}>
-            Products
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={BiSolidCartAdd}>
-            Add product
-          </Sidebar.Item>
-        </Sidebar.ItemGroup>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={BiLogOutCircle}>
-            Sign Out
-          </Sidebar.Item>
-        </Sidebar.ItemGroup>
-      </Sidebar.Items>
-    </Sidebar>
+    <Sidebar aria-label="Sidebar with content separator example" className="fixed w-56 h-screen top-0">
+  <h1 className="text-2xl font-semibold mb-10 ml-4"> <span className="text-green-500">Agro</span>Merce</h1>
+  <Sidebar.Items>
+    <Sidebar.ItemGroup>
+      <Sidebar.Item href="#" icon={HiChartPie}>
+        Dashboard
+      </Sidebar.Item>
+      <Sidebar.Item href="#" icon={HiUser}>
+        Users
+      </Sidebar.Item>
+      <Sidebar.Item href="#" icon={HiShoppingBag}>
+        Products
+      </Sidebar.Item>
+      <Sidebar.Item href="#" icon={BiSolidCartAdd}>
+        Add product
+      </Sidebar.Item>
+    </Sidebar.ItemGroup>
+    <Sidebar.ItemGroup>
+      <Sidebar.Item href="#" icon={BiLogOutCircle}>
+        Sign Out
+      </Sidebar.Item>
+    </Sidebar.ItemGroup>
+  </Sidebar.Items>
+</Sidebar>
+
   );
 }
