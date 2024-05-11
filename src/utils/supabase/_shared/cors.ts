cf898f6a9eec2e74b668bc2c799d828a35150851
mortomulu <mfajrihidayat09@gmail.com>
2024-05-11 23:20:24 +0700

    prod fix : cors policy 5, add client-x & api key cors

--- src/utils/supabase/_shared/cors.ts
+++ src/utils/supabase/_shared/cors.ts
@@ -1,4 +1,4 @@
 export const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': `${process.env.NEXT_SECRET_SUPABASE_JWT}`,
+    'Access-Control-Allow-Headers': `${process.env.NEXT_SECRET_SUPABASE_JWT}, ${process.env.NEXT_SECRET_SUPABASE_SERVICE_ROLE}, ${process.env.NEXT_SECRET_SUPABASE_ANON_KEY}`,
   }
