9b14238f3d57e3153e9de90bfaac89c5f665a3aa
mortomulu <mfajrihidayat09@gmail.com>
2024-05-12 08:04:53 +0700

    prod fix : cors policy 9, change access control header from jwt to anon key

--- src/utils/supabase/_shared/cors.ts
+++ src/utils/supabase/_shared/cors.ts
@@ -1,5 +1,5 @@
 export const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
-    'Access-Control-Allow-Headers': `Bearer ${process.env.NEXT_SECRET_SUPABASE_JWT}`,
+    'Access-Control-Allow-Headers': `Bearer ${process.env.NEXT_SECRET_SUPABASE_ANON_KEY}`,
   }
