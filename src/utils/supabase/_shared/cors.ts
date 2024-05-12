a4f1538c912b1f52862801f0383c4842f7f62d2b
mortomulu <mfajrihidayat09@gmail.com>
2024-05-12 07:35:27 +0700

    prod fix : cors policy 7, bearer auth headers

--- src/utils/supabase/_shared/cors.ts
+++ src/utils/supabase/_shared/cors.ts
@@ -1,4 +1,5 @@
 export const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
+    'Access-Control-Allow-Headers': `Bearer ${process.env.NEXT_SECRET_SUPABASE_JWT}, ${process.env.NEXT_SECRET_SUPABASE_SERVICE_ROLE}, ${process.env.NEXT_SECRET_SUPABASE_ANON_KEY}`,
   }
