31a700199ee833aab6b177578b07f4fa1fe39fc9
mortomulu <mfajrihidayat09@gmail.com>
2024-05-14 19:41:15 +0700

    fix prod : some bug

--- src/app/admin/all-product/page.tsx
+++ src/app/admin/all-product/page.tsx
@@ -1,20 +1,18 @@
-"use client";
-
 import TableAllProduct from "@/components/Table/TableAllProduct";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const AllProductPage = () => {
-  const router = useRouter();
+  // const router = useRouter();
 
-  const { data: session, status }: { data: any; status: string } = useSession();
+  // const { data: session, status }: { data: any; status: string } = useSession();
 
-  useEffect(() => {
-    if (status === "unauthenticated" && session?.user.role !== "admin") {
-      router.push("/");
-    }
-  }, [router, status]);
+  // useEffect(() => {
+  //   if (status === "unauthenticated" && session?.user.role !== "admin") {
+  //     router.push("/");
+  //   }
+  // }, [router, status]);
 
   return (
     <div className="h-auto flex">
